8c076e86880541eb2194319de2d95e2c58de0ea4
jodealter <402048692@qq.com>
2023-03-01 22:04:06 +0800

    v0.1

--- znet/serve.go
+++ znet/serve.go
@@ -1,7 +1,39 @@
 package znet
 
-import "fmt"
+import "github.com/jodealter/zinx/ziface"
 
-func Print() {
-	fmt.Println("kdsssefrefjhbrv")
+type Server struct {
+
+	//服务器名称
+	Name string
+
+	//服务器绑定的版本
+	IPVersion string
+
+	//服务器监听的ip
+	IP string
+
+	//服务器监听的端口
+	Port int
+}
+
+func (s *Server) Start() {
+
+}
+
+func (s *Server) Stop() {
+
+}
+
+func (s *Server) Serve() {
+
+}
+func NewServr(name string) ziface.IServer {
+	s := &Server{
+		Name:      name,
+		IPVersion: "tcp4",
+		IP:        "0.0.0.0",
+		Port:      8999,
+	}
+	return s
 }
